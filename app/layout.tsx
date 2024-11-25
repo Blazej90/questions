ad82ccbbdb117ea02a97bd0b8068a3ae624ca9d3
Błażej Bartoszewski <blazejbart@gmail.com>
2024-11-25 22:23:38 +0100

    add styles to container questions

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+            rel="stylesheet"
+          />
+        </head>
         <body>
           <header>
             <SignedOut>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import SpeechButton from "./components/SpeechButton/SpeechButton";
 import Questions from "./components/Questions/Questions";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Questions and Answers</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Questions and Answers</h1>
       <Questions />
       <SpeechButton />
     </div>
