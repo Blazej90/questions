de9788ffda57d416c8631ac2b1e66b0cf1e2659a
Błażej Bartoszewski <blazejbart@gmail.com>
2024-11-27 11:24:59 +0100

    add styles to sing button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,44 @@
+// import {
+//   ClerkProvider,
+//   SignInButton,
+//   SignedIn,
+//   SignedOut,
+//   UserButton,
+// } from "@clerk/nextjs";
+// import "./globals.css";
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <ClerkProvider>
+//       <html lang="en">
+//         <head>
+//           <link
+//             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+//             rel="stylesheet"
+//           />
+//         </head>
+//         <body>
+//           <header>
+//             <SignedOut>
+//               <SignInButton />
+//             </SignedOut>
+//             <SignedIn>
+//               <div className="user-button">
+//                 <UserButton />
+//               </div>
+//             </SignedIn>
+//           </header>
+//           <main>{children}</main>
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   );
+// }
+
 import {
   ClerkProvider,
   SignInButton,
@@ -22,9 +63,11 @@ export default function RootLayout({
           />
         </head>
         <body>
-          <header>
+          <header className="header-container">
             <SignedOut>
-              <SignInButton />
+              <div className="sign-in-container">
+                <SignInButton />
+              </div>
             </SignedOut>
             <SignedIn>
               <div className="user-button">
