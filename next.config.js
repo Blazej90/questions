ba59f8375aa7418a05ab1040dbb599998cfeae59
Błażej Bartoszewski <blazejbart@gmail.com>
2024-12-03 00:05:37 +0100

    new config for nex.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "regenerator-runtime": require.resolve("regenerator-runtime"),
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;
